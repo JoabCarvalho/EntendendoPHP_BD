5e3db83474e4954c7ab07b9814970ce993b1aaf4
Joab Carvalho <carvalho.morais.sales@gmail.com>
2023-02-02 15:49:10 -0300

    Enviando primeiros arquivos ao diretório.

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelé Gás</title>
+    <style>
+        body{
+            font-family: Georgia;
+            background: linear-gradient(to right, #FFB266, #FF8000);
+            text-align: center;
+            color: white;
+        }
+        .box{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 20px;
+            border-radius: 10px;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+            border: 3px solid dodgerblue;
+            border-radius: 8px;
+            padding: 8px;
+            font-size: 15px;
+        }
+        a:hover{
+            background-color: dodgerblue;
+        }
+        .titulo{
+            font-size: 50px;            
+        }
+        h2{
+            top: 10%;
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="titulo">
+    <h1>Pelé Gás</h1>
+    </div>
+    <div>
+    <h2>O gás da sua cozinha!</h2>
+    </div>
+    <div class="box">
+    <a href="formulario.php">Cadastre-se</a>
+    <br><br><br>
+    <a href="produtos.php">Nossos Produtos</a>
+    </div>
+</body>
+</html>
